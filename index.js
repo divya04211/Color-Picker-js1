2e985ae666ba1bd64046b768a7f3445b804429e2
divya04211 <101892812+divya04211@users.noreply.github.com>
2023-03-09 05:03:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let textColor = document.getElementById('selectedColorHexCode')
+let changeBackGroundColor = document.getElementById('colorPickerContainer')
+
+function button1() {
+    changeBackGroundColor.style.backgroundColor = '#e0e0e0'
+    textColor.textContent = '#e0e0e0'
+}
+
+function button2() {
+    changeBackGroundColor.style.backgroundColor = '#6fcf97'
+    textColor.textContent = '#6fcf97'
+
+}
+
+function button3() {
+    changeBackGroundColor.style.backgroundColor = '#56ccf2'
+    textColor.textContent = '#56ccf2'
+}
+
+function button4() {
+    changeBackGroundColor.style.backgroundColor = '#bb6bd9'
+    textColor.textContent = '#bb6bd9'
+}
